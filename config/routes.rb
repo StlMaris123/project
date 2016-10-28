fed3d77dc1a4bfb4c6cab6940de747b163e3f628
StlMaris123 <stellamarisnjage@gmail.com>
2016-10-28 20:19:33 +0300

    add navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'fixed_pages#home'
-  get 'fixed_pages/help'
+  get  'help' => 'fixed_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
