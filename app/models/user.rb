042b5d3d4eaeb7b7e22323293c5234426a0fe5ff
StlMaris123 <stellamarisnjage@gmail.com>
2016-11-02 11:10:02 +0300

    validate length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence:true
-  validates :phone_number, presence:true
+  validates :name,         presence: true, length: { maximum: 50 }
+  validates :email,        presence: true, length: { maximum: 255 }
+  validates :phone_number, presence: true, length: { minimum: 1 }
 end
