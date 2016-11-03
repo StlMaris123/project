df66d2de0231dd2fd9b761b05f944eff4dfd3312
StlMaris123 <stellamarisnjage@gmail.com>
2016-11-03 08:52:59 +0300

    display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'fixed_pages#home'
   get  'help' => 'fixed_pages#help'
+  get 'signup' => 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
