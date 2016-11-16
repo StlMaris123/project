b3003dedd6864bf007655186ab778d1017182716
StlMaris123 <stellamarisnjage@gmail.com>
2016-11-16 21:13:05 +0300

    add resource for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'fixed_pages#home'
   get  'help' => 'fixed_pages#help'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
