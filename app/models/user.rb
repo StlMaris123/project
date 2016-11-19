ccca33461b5aeabb279981d7adf2f2d60b148d72
StlMaris123 <stellamarisnjage@gmail.com>
2016-11-19 21:55:42 +0300

    add digest method for users in fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,15 @@ class User < ApplicationRecord
   validates :name,         presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,        presence: true, length: { maximum: 255 },
-                           format: { with: VALID_EMAIL_REGEX }, 
-                           uniqueness: { case_sensitive: false }
-   validates :phone_number, presence: true
-   has_secure_password
-   validates :password, length: { minimum: 6 }
+    format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
+  validates :phone_number, presence: true
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
