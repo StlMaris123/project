fd61aef67194084b1024566f4d210c6a01625905
StlMaris123 <stellamarisnjage@gmail.com>
2016-11-21 19:03:45 +0300

    boolean method for login status inside tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome #{@user.name}, make the best out of it"
       redirect_to @user
     else
