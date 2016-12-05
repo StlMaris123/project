84d976022048e321fa4a7ae36b2436cad2b19166
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-05 18:09:21 +0300

    test for successful edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }
   validates :phone_number, presence: true
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -15,4 +15,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "bow"}}
     assert_template 'users/edit'
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Stella Njage"
+    email = "stella@njage.com"
+    phone_number = "0123456"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                               phone_number: phone_number,
+                                               password: "",
+                                               password_confirmation: ""} }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+    assert_equal @user.phone_number, phone_number
+  end
 end
