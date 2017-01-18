c76bf9583897d01d9def7ed44155cb1ea8cec2a9
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-18 19:34:30 +0300

    implement user.followers using passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   depend: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,         presence: true, length: { maximum: 50 }
@@ -37,4 +42,19 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  # follows a user
+  def follow(other_user)
+    following << other_user
+  end
+
+  # unfollows a user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
+
