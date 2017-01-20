845ea18716dc5d6a4d35e4cd682fdd775b2bdbf3
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-20 09:43:38 +0300

    form for following and unfollowing users

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
     end
   end
   resources :users
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
