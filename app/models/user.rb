4d21a965c6f159ff286cc8c53ba10b3ce2e33123
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-21 16:41:01 +0300

    implement the status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent: :destroy
+    foreign_key: "follower_id",
+    dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship",
-                                   foreign_key: "followed_id",
-                                   dependent: :destroy
+    foreign_key: "followed_id",
+    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
@@ -43,6 +43,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # Returns a users's status feed
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                                                              OR user_id = :user_id", user_id: id)
+  end
   # follows a user
   def follow(other_user)
     following << other_user
