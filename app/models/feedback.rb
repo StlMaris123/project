dc0f48889b36269d7320d5b74740d5f4aa9571f9
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-24 20:20:40 +0300

    feedback validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,7 @@
 class Feedback < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :learnt, presence: true
+  validates :problem, presence: true
+  validates :possible_fix, presence: true
 end
