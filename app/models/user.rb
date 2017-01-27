89a90519a73ad83db2a09b4b80460616ca4c69c4
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-27 09:38:30 +0300

    add account activation code to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   has_many :microposts, dependent: :destroy
   has_many :feedbacks, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
@@ -10,7 +13,6 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
-  before_save { self.email = email.downcase }
   validates :name,         presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,        presence: true, length: { maximum: 255 },
@@ -64,6 +66,17 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124170255) do
+ActiveRecord::Schema.define(version: 20170127062026) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "learnt"
@@ -47,11 +47,14 @@ ActiveRecord::Schema.define(version: 20170124170255) do
     t.string   "name"
     t.string   "email"
     t.string   "phone_number"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
