e93b94d92e67434261e7693082e74e82f660e0f5
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-30 16:17:07 +0300

    tests for user signup with account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,31 +1,52 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
-                              phone_number: "",
-                               email: "me@com",
-                              password: "rain",
-                              password_confirmation: "bow"}}
+                                         phone_number: "",
+                                         email: "me@com",
+                                         password: "rain",
+                                         password_confirmation: "bow"}}
     end
     assert_template 'users/new'
   end
-  test "valid signup information" do
+
+  test "valid signup information with account action" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Stella Njage",
-                                        phone_number: "123456",
-                                        email: "ste@njage.com",
-                                        password: "rainbow",
-                                        password_confirmation: "rainbow"
-         
+                                         phone_number: "123456",
+                                         email: "ste@njage.com",
+                                         password: "rainbow",
+                                         password_confirmation: "rainbow"
+
       }}
-      # assert_redirected_to @user
-      # follow_redirect!
     end
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation.
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
+
 end
