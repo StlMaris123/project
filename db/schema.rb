efee1960d0d9fd06f7a8b5d6e07b3b3aeed9f56e
StlMaris123 <stellamarisnjage@gmail.com>
2017-02-08 15:09:17 +0300

    password reset view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127062026) do
+ActiveRecord::Schema.define(version: 20170208114021) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "learnt"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170127062026) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
