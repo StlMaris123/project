8f7cf6474ea784ffcb408ef88eea458a167640ec
StlMaris123 <stellamarisnjage@gmail.com>
2017-04-11 16:58:04 +0300

    view for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,28 @@ class CommentsController < ApplicationController
   before_action :find_commentable
 
   def new
+    @micropost = Micropost.find(params[:id])
     @comment = Comment.new
   end
 
- def create
-   @comment = @ccommentable.comments.new comment_params
-   if @comment.save
-     redirect_to :back, notice: 'comment successfully posted'
-   else
-     redirect_to :back, notice: 'error posting comment'
-   end
- end
+  def create
+    @comment = @commentable.comments.new comment_params
+    if @comment.save
+      redirect_to :back, notice: 'comment successfully posted'
+    else
+      redirect_to :back, notice: 'error posting comment'
+    end
+  end
 
- private
+  private
 
- def comment_params
-   params.require(:comment).permit(:body)
- end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
- def find_commentable
-   @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-   @commentable = Micropost.find_by_id(params[:micropost_id]) if params[:micropost_id]
- end
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Micropost.find_by_id(params[:micropost_id]) if params[:micropost_id]
+  end
 
 end
