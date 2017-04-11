28bd2a7e276d59755ad106a77020a907f0d2bf9f
StlMaris123 <stellamarisnjage@gmail.com>
2017-04-11 20:48:47 +0300

    assignment create action

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,4 +2,20 @@ class AssignmentsController < ApplicationController
   def new
     @assignment = Assignment.new
   end
+
+  def create
+    @assignment = Assignment.new(assignment_params)
+    if @assignment.save
+      redirect_to new_assignment_path
+      flash[:success] = "Assignment created"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def assignment_params
+    params.require(:assignment).permit(:title, :description, :link, :start_date, :due_date)
+  end
 end
