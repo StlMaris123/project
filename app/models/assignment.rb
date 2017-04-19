256cf3297a8a035de67907f10e926eaeedb103be
StlMaris123 <stellamarisnjage@gmail.com>
2017-04-19 19:22:28 +0300

    style home page

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,4 @@
 class Assignment < ApplicationRecord
 validates :title, presence: true
-validates_format_of :link, :with => URI.regexp
-validates :link, presence: true
+validates_format_of :link, :with => URI.regexp, allow_blank: true
 end
